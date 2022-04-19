64a07096b65a2eba372e9197e734d3d9320fb6df
Dylan FEITH <dylan.feith@keenobi.com>
2022-04-19 15:25:35 +0200

    fix elasticache redis remove sg

--- modules/elasticache-redis/outputs.tf
+++ modules/elasticache-redis/outputs.tf
@@ -28,46 +28,6 @@ output "elasticache_parameter_group_id" {
   description = "The ElastiCache parameter group name."
 }
 
-output "security_group_id" {
-  value       = aws_security_group.redis.id
-  description = "The ID of the Redis ElastiCache security group."
-}
-
-output "security_group_arn" {
-  value       = aws_security_group.redis.arn
-  description = "The ARN of the Redis ElastiCache security group."
-}
-
-output "security_group_vpc_id" {
-  value       = aws_security_group.redis.vpc_id
-  description = "The VPC ID of the Redis ElastiCache security group."
-}
-
-output "security_group_owner_id" {
-  value       = aws_security_group.redis.owner_id
-  description = "The owner ID of the Redis ElastiCache security group."
-}
-
-output "security_group_name" {
-  value       = aws_security_group.redis.name
-  description = "The name of the Redis ElastiCache security group."
-}
-
-output "security_group_description" {
-  value       = aws_security_group.redis.description
-  description = "The description of the Redis ElastiCache security group."
-}
-
-output "security_group_ingress" {
-  value       = aws_security_group.redis.ingress
-  description = "The ingress rules of the Redis ElastiCache security group."
-}
-
-output "security_group_egress" {
-  value       = aws_security_group.redis.egress
-  description = "The egress rules of the Redis ElastiCache security group."
-}
-
 output "elasticache_auth_token" {
   description = "The Redis Auth Token."
   value       = aws_elasticache_replication_group.redis.auth_token
